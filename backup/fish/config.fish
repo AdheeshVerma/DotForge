a1c28d12cd42954f0f4a302db2166242ba8e4c65
Adheesh Verma <adheeshzkp@gmail.com>
2026-08-17 23:30:11 +0530

    Edited Fastfetch config

--- backup/fish/config.fish
+++ backup/fish/config.fish
@@ -169,7 +169,7 @@ alias rip 'expac --timefmt="%Y-%m-%d %T" "%l\t%n %v" | sort | tail -200 | nl'
 
 ## Run fastfetch if session is interactive
 if status --is-interactive && type -q fastfetch
-   fastfetch --config neofetch.jsonc
+   fastfetch 
     #  fastfetch --logo arch
 end
 
